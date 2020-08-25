cc53374c0a5c082bd439ca919fa68fd09b23d031
yerx <yer.x.xiong@gmail.com>
2020-08-25 08:45:48 -0400

    updated code

--- src/Travel.java
+++ src/Travel.java
@@ -4,13 +4,12 @@ public class Travel {
         double populationSize = 49.7;
         String capital = "Nairobi";
         double currency = 103.84;
-        String currencyName = "Kenyan Shillings";
 
         System.out.println("Hey Mom and Dad!" + "\n" + "Here are some interesting details about the first country I'm visiting.");
         System.out.println("Name: " + country);
         System.out.println("Population Size: " + populationSize + " million");
         System.out.println("Capital: " + capital);
-        System.out.println("Currency: One U.S. Dollar equals $" + currency + " " + currencyName + "!!");
+        System.out.println("Currency: One U.S. Dollar equals $" + currency + " " + "Kenyan Shillings!!");
         System.out.println("I'm so excited to visit the Nairobi National Park and feel the giraffes!! Talk later.");
     }
 }
